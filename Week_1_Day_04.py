483b4d65eb7d8d008b693f62a7ac5beb8744228b
Vikash Kumar <er.vikashs4@gmail.com>
2020-06-04 15:17:26 +0530

    Create Week 1 day 4 Solution

---
+++ Week_1_Day_04.py
@@ -0,0 +1,31 @@
+Q.) Write a function that reverses a string. The input string is given as an array of characters char[].
+
+Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+You may assume all the characters consist of printable ascii characters.
+
+ 
+
+Example 1:
+
+Input: ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"]
+Example 2:
+
+Input: ["H","a","n","n","a","h"]
+Output: ["h","a","n","n","a","H"]
+
+
+This Approcah also think:  The entire logic for reversing a string is based on using the opposite directional two-pointer approach!
+
+
+Solution.................................................................
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        s.reverse()
+
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        
